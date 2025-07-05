bec2c853f96bdadb91922667eec29c17bfd7838b
manas baranwal <manas.baranwal13@gmail.com>
2025-07-05 22:26:30 +0530

    variable changes in js

---
+++ part1/changes.js
@@ -0,0 +1,10 @@
+let gameName = "spider-man"
+console.log(gameName);
+
+gameName = "Batman"
+console.log(gameName) //possible
+
+const username = ("manas")
+
+const username = ("manas123")
+// console.log(username) gives you error
