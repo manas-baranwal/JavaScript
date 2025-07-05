e15f7b81024398e94f2265adea98d4266f25e394
manas baranwal <manas.baranwal13@gmail.com>
2025-07-05 20:04:58 +0530

    data types

---
+++ part1/DataType.js
@@ -0,0 +1,13 @@
+// integer
+var score = 102 //Old Way
+let a = 1 //New way
+
+// string
+let name = "Manas"
+
+// boolean
+let isLoggedin = true
+
+// Object
+let tea = ["iced tea","hot tea"]
+let name = {firstname:"Manas",surname:"Baranwal"}
