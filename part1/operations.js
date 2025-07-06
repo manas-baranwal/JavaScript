c7fbc91f58703a7e06f960f08f438e034ab8df74
manas baranwal <manas.baranwal13@gmail.com>
2025-07-07 01:34:54 +0530

    arthematic operations

---
+++ part1/operations.js
@@ -0,0 +1,32 @@
+
+let score = 120
+let bonus = 25
+let total = score+bonus
+console.log(total) //indirect addition
+
+let addtion = 5+5
+console.log(addtion);
+
+let sub = 9-2
+console.log(sub)
+
+let multiplication = 2*3
+console.log(multiplication)
+
+let divide = 5/2
+console.log(divide)
+
+let reminder = 5%2
+console.log(reminder)
+
+let exp = 3**3
+console.log(exp)
+
+let myscore = 110
+myscore++
+console.log(myscore)
+
+let credit = 56
+credit--
+console.log(credit)
+
