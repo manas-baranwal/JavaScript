279d84e548a0ec26568ca390d016188db67af66b
manas baranwal <manas.baranwal13@gmail.com>
2025-08-17 21:30:45 +0530

    basic loops question

---
+++ part4/loops.js
@@ -0,0 +1,19 @@
+ // to add numbers from 1 - 5
+
+ let sum = 0
+ let i = 1
+ while (i<=5) {
+    sum+=i
+    i++
+ }
+ console.log(sum);
+ 
+// to sotre value from 5 - 1 in an array
+
+countdown = []
+let j = 5
+while (j>0) {
+    countdown.push(j)
+    j--
+}
+console.log(countdown)
